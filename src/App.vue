4e211ad6ec74f518183be2041cfdfd938ea5ec1c
David Sturtz <dsturtz@gmail.com>
2026-05-23 18:05:15 -0500

    Initial commit with Vue and Vuetify

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import BottomNav from './components/BottomNav.vue'
+
+</script>
+
+<template>
+<v-app>
+    <h1>Welcome</h1>
+  <v-main>
+    <RouterView />
+  </v-main>
+    <BottomNav />
+  </v-app>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import Home from '../components/Home.vue'
+</script>
+
+<template>
+  <main>
+  <h1>Hello?</h1>
+    <Home />
+  </main>
+</template>
